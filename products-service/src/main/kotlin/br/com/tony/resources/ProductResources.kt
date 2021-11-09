a77c22460e3c59c1076bbd2d91f8bae1f8483e44
tonyals <tony_luzsilva@hotmail.com>
2021-11-09 20:07:31 -0300

    feat: define contract .proto

--- products-service/src/main/kotlin/br/com/tony/resources/ProductResources.kt
+++ products-service/src/main/kotlin/br/com/tony/resources/ProductResources.kt
@@ -1,21 +1,14 @@
 package br.com.tony.resources
 
-import br.com.tony.ProductsServiceReply
-import br.com.tony.ProductsServiceRequest
-import br.com.tony.ProductsServiceServiceGrpc
+import br.com.tony.ProductServiceRequest
+import br.com.tony.ProductServiceResponse
+import br.com.tony.ProductsServiceGrpc
 import io.grpc.stub.StreamObserver
 import io.micronaut.grpc.annotation.GrpcService
 
 @GrpcService
-class ProductResources : ProductsServiceServiceGrpc.ProductsServiceServiceImplBase() {
-    override fun send(request: ProductsServiceRequest?, responseObserver: StreamObserver<ProductsServiceReply>?) {
-        val toSend = "Hello, ${request?.name}"
-
-        val reply = ProductsServiceReply.newBuilder()
-            .setMessage(toSend)
-            .build()
-
-        responseObserver?.onNext(reply)
-        responseObserver?.onCompleted()
+class ProductResources : ProductsServiceGrpc.ProductsServiceImplBase() {
+    override fun create(request: ProductServiceRequest?, responseObserver: StreamObserver<ProductServiceResponse>?) {
+        super.create(request, responseObserver)
     }
 }
