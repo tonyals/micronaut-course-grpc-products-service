daa0fd188494c6ec3e4c6ee3ddccd9788eb3b3a4
tonyals <tony_luzsilva@hotmail.com>
2021-11-10 18:55:37 -0300

    feat: implements create product in ProductResources.kt

--- products-service/src/main/kotlin/br/com/tony/repository/ProductRepository.kt
+++ products-service/src/main/kotlin/br/com/tony/repository/ProductRepository.kt
@@ -1,8 +1,8 @@
 package br.com.tony.repository
 
 import br.com.tony.domain.Product
+import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
-import jakarta.inject.Singleton
 
-@Singleton
+@Repository
 interface ProductRepository : JpaRepository<Product, Long>
--- products-service/src/main/kotlin/br/com/tony/resources/ProductResources.kt
+++ products-service/src/main/kotlin/br/com/tony/resources/ProductResources.kt
@@ -3,12 +3,24 @@ package br.com.tony.resources
 import br.com.tony.ProductServiceRequest
 import br.com.tony.ProductServiceResponse
 import br.com.tony.ProductsServiceGrpc
+import br.com.tony.dto.ProductReq
+import br.com.tony.services.ProductService
 import io.grpc.stub.StreamObserver
 import io.micronaut.grpc.annotation.GrpcService
 
 @GrpcService
-class ProductResources : ProductsServiceGrpc.ProductsServiceImplBase() {
+class ProductResources(private val productService: ProductService) : ProductsServiceGrpc.ProductsServiceImplBase() {
     override fun create(request: ProductServiceRequest?, responseObserver: StreamObserver<ProductServiceResponse>?) {
-        super.create(request, responseObserver)
+        val productReq = ProductReq(name = request!!.name, price = request.price, quantityInStock = request.quantityInStock)
+        val productRes = productService.create(productReq)
+        val productResponse = ProductServiceResponse.newBuilder()
+            .setId(productRes.id)
+            .setName(productRes.name)
+            .setPrice(productRes.price)
+            .setQuantityInStock(productRes.quantityInStock)
+            .build()
+
+        responseObserver?.onNext(productResponse)
+        responseObserver?.onCompleted()
     }
 }
