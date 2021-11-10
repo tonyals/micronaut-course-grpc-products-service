4e5c7b198c17a28a8a04db64ed39eb2bea21899b
tonyals <tony_luzsilva@hotmail.com>
2021-11-09 21:09:35 -0300

    feat: create domain Product and dtos ProductReq.kt ProductRes.kt

---
+++ products-service/src/main/kotlin/br/com/tony/domain/Product.kt
@@ -0,0 +1,16 @@
+package br.com.tony.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Product(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    val name: String,
+    val price: Double,
+    val quantityInStock: Int
+)
---
+++ products-service/src/main/kotlin/br/com/tony/dto/ProductReq.kt
@@ -0,0 +1,7 @@
+package br.com.tony.dto
+
+data class ProductReq(
+    val name: String,
+    val price: Double,
+    val quantityInStock: Int
+)
