8bb5c4ad253276b50c5969c62d55df938d7b021b
tonyals <tony_luzsilva@hotmail.com>
2021-11-09 21:59:03 -0300

    feat: initial implementation of create service

--- products-service/src/main/kotlin/br/com/tony/services/impl/ProductServiceImpl.kt
+++ products-service/src/main/kotlin/br/com/tony/services/impl/ProductServiceImpl.kt
@@ -2,12 +2,16 @@ package br.com.tony.services.impl
 
 import br.com.tony.dto.ProductReq
 import br.com.tony.dto.ProductRes
+import br.com.tony.repository.ProductRepository
 import br.com.tony.services.ProductService
+import br.com.tony.util.toDomain
+import br.com.tony.util.toProductRes
 import jakarta.inject.Singleton
 
 @Singleton
-class ProductServiceImpl : ProductService {
+class ProductServiceImpl(private val productRepository: ProductRepository) : ProductService {
     override fun create(req: ProductReq): ProductRes {
-        TODO("Not yet implemented")
+        val productSaved = productRepository.save(req.toDomain())
+        return productSaved.toProductRes()
     }
 }
---
+++ products-service/src/main/kotlin/br/com/tony/util/ProductUtils.kt
@@ -0,0 +1,23 @@
+package br.com.tony.util
+
+import br.com.tony.domain.Product
+import br.com.tony.dto.ProductReq
+import br.com.tony.dto.ProductRes
+
+fun Product.toProductRes(): ProductRes {
+    return ProductRes(
+        id = id!!,
+        name = name,
+        price = price,
+        quantityInStock = quantityInStock
+    )
+}
+
+fun ProductReq.toDomain(): Product {
+    return Product(
+        id = null,
+        name = name,
+        price = price,
+        quantityInStock = quantityInStock
+    )
+}
